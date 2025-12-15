fb8fccae9dabe90b8a7c4caeb9220eb0740672ff
Sarthak-0108 <sarthakkumar0108@gmail.com>
2025-12-15 22:24:03 +0530

    schedule component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import Todo from "./components/Todo";
 import { useState, useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
+  const dateInputRef = useRef(null);
+  const handleContainerClick = () => {
+    if (dateInputRef.current) {
+      if (dateInputRef.current.showPicker) {
+        dateInputRef.current.showPicker();
+      } else {
+        dateInputRef.current.focus();
+      }
+    }
+  };
+
   const [inputValue, setinputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const [folder, setFolder] = useState("today");
@@ -54,13 +67,32 @@ function App() {
     );
   };
   return (
-    <div className="max-w-xl mx-auto px-4">
+    <div className="max-w-3xl mx-auto px-4">
       <h1 className="text-center text-3xl md:text-5xl m-8">
         <span className="text-blue-950 bold text-6xl md:text-8xl">π</span>Todo
         List
       </h1>
 
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+        <button
+          onClick={handleContainerClick}
+          className="relative flex items-center justify-center px-4 py-2 text-sm md:text-lg font-semibold bg-slate-800 text-white rounded-md hover:bg-slate-900 cursor-pointer"
+        >
+          <FontAwesomeIcon
+            icon={faCalendar}
+            className="text-gray-500 mr-2 pointer-events-none" // Ensure icon doesn't block click
+          />
+
+          <span className="pointer-events-none">Schedule</span>
+
+          {/* 4. The Hidden Input */}
+          {/* We hide it visually but keep it in the DOM */}
+          <input
+            ref={dateInputRef}
+            type="date"
+            className="absolute w-0 h-0 opacity-0" // Hide it completely
+          />
+        </button>
         <div className="flex items-center border border-gray-900 rounded px-3 py-2 flex-grow">
           <FontAwesomeIcon
             icon={faPlus}
@@ -91,7 +123,7 @@ function App() {
         </select>
       </div>
 
-      <div className="mt-6 flex flex-col gap-2">
+      <div className=" w-full mt-6 flex flex-col gap-2 md:ml-[19%]">
         {getFilteredTodos().map((todo) => (
           <Todo
             key={todo.id}
